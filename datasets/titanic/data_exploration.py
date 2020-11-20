60552398d686c6f9aa4fa5c7c9ef9968070a177d
Raul <rchfe23@gmail.com>
2020-11-20 10:32:11 -0500

    data_exploration: just call the relevant methods from dautils

--- datasets/titanic/data_exploration.py
+++ datasets/titanic/data_exploration.py
@@ -16,65 +16,19 @@ Dataset website: https://www.kaggle.com/c/titanic
 import logging.config
 from logging import NullHandler
 
-import ipdb
-import pandas as pd
-
-from datasets.my_utils import dautils as da, genutils as ge
-from datasets.titanic import (__name__ as package_name,
-                              __path__ as package_path,
-                              __version__ as package_version)
-
+import data_exploration
+from datasets import titanic
+from datasets.my_utils import dautils as da
 logger = logging.getLogger(__name__)
 logger.addHandler(NullHandler())
 
 
 def main():
     global logger
-    # ============================
-    # Parse command-line arguments
-    # ============================
-    parser = ge.setup_argparser(package_version)
-    args = parser.parse_args()
-    cfg_filepath, log_filepath = ge.get_cfg_filepaths(args)
-    # Get config dict
-    cfg_dict = ge.load_cfg_dict(cfg_filepath)
-
-    # ==============================
-    # Logging setup from config file
-    # ==============================
-    # NOTE: if quiet and verbose are both activated, only quiet will have an effect
-    if cfg_dict['quiet']:
-        # TODO: disable logging completely? even error messages?
-        logger.disabled = True
-    else:
-        log_dict = ge.load_cfg_dict(log_filepath, is_logging=True)
-        if cfg_dict['verbose']:
-            ge.set_logging_level(log_dict)
-        logging.config.dictConfig(log_dict)
-        logger = logging.getLogger(ge.get_logger_name(__name__, package_name, __file__))
-
-    # =============
-    # Start logging
-    # =============
-    logger.info("Running {} v{}".format(package_name, package_version))
-    logger.debug("Package path: {}".format(package_path[0]))
-    logger.info("Verbose option {}".format(
-        "enabled" if cfg_dict['verbose'] else "disabled"))
-
-    # ---------
-    # Load data
-    # ---------
-    logger.info("Loading data")
-    train = pd.read_csv(cfg_dict['train_file_path'])
-    test = pd.read_csv(cfg_dict['test_file_path'])
-
-    # --------------------
-    # Compute simple stats
-    # --------------------
-    if cfg_dict['compute_simple_stats']['for_train']:
-        da.compute_simple_stats(train, 'train', excluded_cols=['PassengerId'])
-    if cfg_dict['compute_simple_stats']['for_test']:
-        da.compute_simple_stats(test, 'test', excluded_cols=['PassengerId'])
+    data = da.DataExplorer(titanic, data_exploration)
+    logger = data.module_logger
+    data.compute_simple_stats()
+    data.head()
 
 
 if __name__ == '__main__':
