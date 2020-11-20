55a2e10f811414c7252099e289a142ab8cb99192
Raul <rchfe23@gmail.com>
2020-11-20 10:30:40 -0500

    config.py: add filepaths, for_* and *_head

--- datasets/titanic/configs/config.py
+++ datasets/titanic/configs/config.py
@@ -1,11 +1,20 @@
 quiet = False
 verbose = True
 
-train_file_path = '~/Data/kaggle_datasets/titanic/train.csv'
-test_file_path = '~/Data/kaggle_datasets/titanic/test.csv'
+data_filepath = ''
+train_filepath = '~/Data/kaggle_datasets/titanic/train.csv'
+valid_filepath = ''
+test_filepath = '~/Data/kaggle_datasets/titanic/test.csv'
 
 compute_simple_stats = {
+    'for_data': True,
     'for_train': True,
+    'for_valid': True,
     'for_test': True,
     'excluded_cols': ['PassengerId']
 }
+
+data_head = 5
+train_head = 5
+valid_head = 5
+test_head = 5
