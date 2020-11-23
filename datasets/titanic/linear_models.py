a7cf47beb4ed77dc5d303b202f21abcf75b673d3
Raul <rchfe23@gmail.com>
2020-11-22 21:06:22 -0500

    Update linear_models.py

--- datasets/titanic/linear_models.py
+++ datasets/titanic/linear_models.py
@@ -3,12 +3,17 @@
 import logging.config
 from logging import NullHandler
 
+from sklearn.linear_model import LogisticRegression
+
 logger = logging.getLogger(__name__)
 logger.addHandler(NullHandler())
 
 
 def main():
     global logger
+
+    # Load the data
+
 
 
 if __name__ == '__main__':
