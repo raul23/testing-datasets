a6e19e6bab9c869180fd7cde1c1883886d16d26f
Raul <rchfe23@gmail.com>
2020-11-22 21:41:58 -0500

    config: flat options only and separate options with headings

--- datasets/titanic/configs/config.py
+++ datasets/titanic/configs/config.py
@@ -1,31 +1,52 @@
+# ---------------
+# General options
+# ---------------
 quiet = False
 verbose = True
 
-data_filepath = ''
+# --------------
+# Data filepaths
+# --------------
+data_filepath = None
 train_filepath = '~/Data/kaggle_datasets/titanic/train.csv'
-valid_filepath = ''
+valid_filepath = None
 test_filepath = '~/Data/kaggle_datasets/titanic/test.csv'
+y_target = 'Survived'
 
-compute_stats = {
-    'for_data': True,
-    'for_train': True,
-    'for_valid': True,
-    'for_test': True,
-    'excluded_cols': ['PassengerId']
-}
+# -------------
+# Compute stats
+# -------------
+data_stats= True,
+train_stats= True,
+valid_stats= True,
+test_stats= True,
+excluded_cols= ['PassengerId']
 
+# ------------------
+# HEAD: first N rows
+# ------------------
 data_head = 5
 train_head = 5
 valid_head = 5
 test_head = 5
 
+# ------------------------------
+# Count number of missing values
+# ------------------------------
 data_isnull = True
 train_isnull = True
 valid_isnull = True
 test_isnull = True
 
+# ----------------
+# Tree-based model
+# ----------------
 trees_models = {
     'model_name': 'RandomForestClassifier',
     'model_params': {
-    }
+        'n_estimators': 100,
+        'max_depth': 5,
+        'random_state': 1
+    },
+    'features': ["Pclass", "Sex", "SibSp", "Parch"]
 }
