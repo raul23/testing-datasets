c08aa7b31555352a1645e95a5f51428f118297fd
Raul <rchfe23@gmail.com>
2020-11-22 23:51:42 -0500

    trees_models: load data, train model, and get preds

--- datasets/titanic/trees_models.py
+++ datasets/titanic/trees_models.py
@@ -4,9 +4,8 @@ import logging.config
 from logging import NullHandler
 
 import ipdb
-# from sklearn.ensemble import RandomForestClassifier
 
-from datasets.my_utils import genutils as ge
+from datasets.my_utils import genutils as ge, mlutils as ml
 
 logger = logging.getLogger(__name__)
 logger.addHandler(NullHandler())
@@ -14,11 +13,24 @@ logger.addHandler(NullHandler())
 
 def main():
     global logger
-    cfg = ge.ConfigBoilerplate(__file__)
-    logger = cfg.get_logger()
-    logger.info("test")
+    bp = ge.ConfigBoilerplate(__file__)
+    cfg_dict = bp.get_cfg_dict()
+    logger = bp.get_logger()
 
-    # Load the data
+    # Load data
+    data = ml.Datasets(**cfg_dict)
+
+    # Get model
+    logger.debug(f"Get model: {cfg_dict['model']['model_type']}")
+    clf = ml.get_model(**cfg_dict['model'])
+
+    # Train and get preds
+    logger.info("Train model")
+    clf.fit(data.X, data.y)
+    score = clf.score(data.X, data.y)
+    logger.info(f"Score on train: {score}")
+    logger.info("Get predictions from test data")
+    predictions = clf.predict(data.X_test)
 
 
 if __name__ == '__main__':
