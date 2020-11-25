c8e5450bf4e23c7c3155e72e393c729dc41ed200
Raul <rchfe23@gmail.com>
2020-11-25 15:31:32 -0500

    mlutils: log message (importing) and add more models)

--- datasets/my_utils/mlutils.py
+++ datasets/my_utils/mlutils.py
@@ -4,9 +4,13 @@ import logging.config
 from logging import NullHandler
 
 import ipdb
-import pandas as pd
+# import pandas as pd
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 
 from datasets.my_utils import genutils as ge
+
+pandas = None
 
 logger = logging.getLogger(ge.get_short_logger_name(__name__))
 logger.addHandler(NullHandler())
@@ -19,26 +23,34 @@ class Datasets:
         # ------------------
         # Parameters parsing
         # ------------------
+        global pandas
+        logger.info("Importing pandas as pd...")
+        import pandas
         self.train_filepath = train_filepath
         self.test_filepath = test_filepath
         self.y_target = y_target
         if features:
-            logger.debug(f"Using these features: {features}")
+            logger.debug(f"Using only these features: {features}")
             self.features = features
         else:
             logger.debug("Using all features")
-            self.features = []
+            self.features = None
         self.get_dummies = get_dummies
         # ------------
         # Data loading
         # ------------
         # Load train data
         logger.info("Loading training data")
-        self.train_data = pd.read_csv(train_filepath)
+        self.train_data = pandas.read_csv(train_filepath)
+        if not self.features:
+            self.features = self.train_data.columns.to_list()
+            # Remove target from features
+            self.features.remove(self.y_target)
+            logger.debug(f"Using all {len(self.features)} features")
         self.y = self.train_data[y_target]
         # Load test data
         logger.info("Loading test data")
-        self.test_data = pd.read_csv(test_filepath)
+        self.test_data = pandas.read_csv(test_filepath)
         # ------------------
         # Data preprocessing
         # ------------------
@@ -53,22 +65,37 @@ class Datasets:
     def _get_dummies(self):
         # Replace missing values on train and test
         logger.info("One-hot encoding the data")
-        self.X = pd.get_dummies(self.X)
-        self.X_test = pd.get_dummies(self.X_test)
+        self.X = pandas.get_dummies(self.X)
+        self.X_test = pandas.get_dummies(self.X_test)
 
 
 # TODO: catch error in models
-def get_model(model_type, model_params, *args, **kwargs):
+def get_clf(model_type, model_params, scale_input=False, *args, **kwargs):
     logger.debug(f"Get model: {model_type}")
     logger.info(f"Importing {model_type}...")
+    if model_type == 'LinearSVC':
+        from sklearn.svm import LinearSVC as clf
     if model_type == 'LogisticRegression':
-        from sklearn.linear_model import LogisticRegression as model
+        from sklearn.linear_model import LogisticRegression as clf
+    elif model_type == 'Perceptron':
+        # Perceptron() is equivalent to SGDClassifier(loss="perceptron", eta0=1,
+        # learning_rate="constant", penalty=None).
+        # Ref.: https://bit.ly/3pPnZDc (sklearn-perceptron)
+        from sklearn.linear_model import Perceptron as clf
     elif model_type == 'RandomForestClassifier':
-        from sklearn.ensemble import RandomForestClassifier as model
+        from sklearn.ensemble import RandomForestClassifier as clf
+    elif model_type == 'SGDClassifier':
+        from sklearn.linear_model import SGDClassifier as clf
+    elif model_type == 'SVC':
+        from sklearn.svm import SVC as clf
     else:
         raise TypeError(f"Model type not supported: {model_type}")
-    return model(**model_params)
-
-
-
-
+    if scale_input:
+        logger.info("Input will be scaled")
+        """
+        logger.info("Importing sklearn.pipeline.make_pipeline")
+        from sklearn.pipeline import make_pipeline
+        """
+        return make_pipeline(StandardScaler(), clf(**model_params))
+    else:
+        return clf(**model_params)
